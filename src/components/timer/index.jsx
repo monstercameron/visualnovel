74b3991650f5a8fb9ef41dba76359a4ab876fbcd
Earl Cameron <mr.e.cameron@gmail.com>
2021-09-25 22:14:54 -0400

    timer bug fixed

--- src/components/timer/index.jsx
+++ src/components/timer/index.jsx
@@ -1,7 +1,7 @@
 import React,{useState, useEffect} from 'react'
 import {loader, outerLoader, loaderPost} from './index.module.css'
 
-export default function Index({time, func}) {
+export default function Index({time, func, reset=true}) {
     const MAX_WIDTH = 407
     const STEPS = MAX_WIDTH / time
     const [count, setCount] = useState(time)
@@ -9,6 +9,7 @@ export default function Index({time, func}) {
     let countInterval = null
 
     const counter = () => {
+        reset = !reset
         if(countInterval === null) {
             countInterval = setTimeout(() => {
                 setCount(count => count - 1)
@@ -21,6 +22,7 @@ export default function Index({time, func}) {
         counter()
        if (count < 0) {
         clearTimeout(countInterval)
+        setCount(time)
         func()
        }
     },[count])
