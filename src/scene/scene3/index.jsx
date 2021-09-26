9c6ba33cf5e40f824912ce2b47e0df2a5b22b84c
Earl Cameron <mr.e.cameron@gmail.com>
2021-09-25 23:43:41 -0400

    push

--- src/scene/scene3/index.jsx
+++ src/scene/scene3/index.jsx
@@ -13,7 +13,8 @@ import Speech from "../../components/speech";
 import Pet from "../../components/pet";
 
 // import imgs
-import heroImg from '../../assets/Arouramain.png'
+import heroImgAur from '../../assets/Arouramain.png'
+import heroImgSky from '../../assets/Sky1.png'
 import bg from '../../assets/Step1.png'
 
 export default function Index() {
@@ -21,7 +22,7 @@ export default function Index() {
 	const test = useContext(Context);
 
     let decisionList = null
-	const [step, setStep] = useState(0);
+	const [step, setStep] = useState(3);
 
     const [choice, setChoice] = useState("");
 	const getChoice = (choice) => {
@@ -31,48 +32,145 @@ export default function Index() {
 	const next = () => {
         // console.log('NEXT!')
 		// history.push("/test");
-		if (step < decisionList.length -1) {
+        if(step === 3){
+            switch(choice){
+                case "In-House Fulfillment":
+                    setStep(step + 1);
+                    break;
+                case "Dropshipping":
+                    //history.push("/scene4");
+                    break;
+                default:
+                    setStep(step + 1);
+                    break;
+                }
+        }else if (step < decisionList.length -1) {
 			setStep(step + 1);
-		}
+		}else{
+		// history.push("/scene5");
+        }
 	};
     
 
-	const decision1 = () => {
+	const dialog1 = () => {
 		return <>
 			<Timer
-				time={5}
+				time={3}
 				func={next}
 			/>
-			<Hero pos={['275px','30px']} img={heroImg} />
-			<Speech facing='left' pos={['300px','300px']} >
+			<Hero pos={['275px','30px']} img={heroImgAur} />
+            
+			<Hero pos={['190px','980px']} img={heroImgSky} />
+			<Speech facing='right' pos={['150px','650px']} >
 				<SpeechLib  pitch="0.9"
 					rate="0.9"
 					volume="0.7"
 					lang="en-GB"
-					voice="Microsoft Zira - English (United States)" text='Halloween is coming and I just might have the perfect idea to get the spooky season rolling. A Halloween pop-up store! Now, to decide, brick and mortar or online?
-					' />
-					Halloween is coming and I just might have the perfect idea to get the spooky season rolling. A Halloween pop-up store! Now, to decide, brick and mortar or online?
+					voice="Microsoft Zira - English (United States)" text='Holy Witch! These boxes are getting so out of hand! Do you have a plan to be able to handle the fulfillment process with an increase of orders when the pop-up store launches?”
+                    Aurora: “That was actually a good question, insubordinate, but a good question. Of course, I have a plan. Why do you think trees have so many friends?.... Because they branch out!' />
+					Holy Witch! These boxes are getting so out of hand! Do you have a plan to be able to handle the fulfillment process with an increase of orders when the pop-up store launches?”
+Aurora: “That was actually a good question, insubordinate, but a good question. Of course, I have a plan. Why do you think trees have so many friends?.... Because they branch out!
+
 			</Speech>
 		</>
 	}
+    
+	const dialog2 = () => {
+		return <>
+			<Timer
+				time={30}
+				func={next}
+			/>
+			<Hero pos={['275px','30px']} img={heroImgAur} />
+            
+			<Hero pos={['190px','980px']} img={heroImgSky} />
+			<Speech facing='right' pos={['150px','650px']} >
+				<SpeechLib  pitch="0.9"
+					rate="0.9"
+					volume="0.7"
+					lang="en-GB"
+					voice="Microsoft Zira - English (United States)" text='Branch out???' />Branch out???</Speech>
+		</>
+	}
+    
+	const dialog3 = () => {
+		return <>
+			<Timer
+				time={30}
+				func={next}
+			/>
+			<Hero pos={['275px','30px']} img={heroImgAur} />
+			<Speech facing='right' pos={['150px','650px']} >
+				<SpeechLib  pitch="0.9"
+					rate="0.9"
+					volume="0.7"
+					lang="en-GB"
+					voice="Microsoft Zira - English (United States)" text='To keep it simple, we can purchase items from a third-party supplier and have them shipped directly to the customer. It’s a fulfillment method called Dropshipping! So either we fulfill our own orders or we can start dropshipping, let’s choose!' />To keep it simple, we can purchase items from a third-party supplier and have them shipped directly to the customer. It’s a fulfillment method called Dropshipping! So either we fulfill our own orders or we can start dropshipping, let’s choose!</Speech>
+            
+			<Hero pos={['190px','980px']} img={heroImgSky} />
+		</>
+	}
+
+    const decision1 = () => {
+        return <>
+        <Timer
+            time={5}
+            func={next}
+        />
+        <Hero pos={['275px','30px']} img={heroImgAur} />
+        <Speech facing='right' pos={['150px','650px']} >
+            <SpeechLib  pitch="0.9"
+                rate="0.9"
+                volume="0.7"
+                lang="en-GB"
+                voice="Microsoft Zira - English (United States)" text='To keep it simple, we can purchase items from a third-party supplier and have them shipped directly to the customer. It’s a fulfillment method called Dropshipping! So either we fulfill our own orders or we can start dropshipping, let’s choose!' />To keep it simple, we can purchase items from a third-party supplier and have them shipped directly to the customer. It’s a fulfillment method called Dropshipping! So either we fulfill our own orders or we can start dropshipping, let’s choose!</Speech>
+        
+        <Hero pos={['190px','980px']} img={heroImgSky} />
+        
+    <Tree options={[
+        'In-House Fulfillment',
+        'Dropshipping'
+    ]} defaultOpt={1} func={getChoice}/>
+        </>
+    }
 
     
+    const stepAdialog1 = () => {
+        return <>
+        <Timer
+            time={5}
+            func={next}
+        />
+        <Hero pos={['275px','30px']} img={heroImgAur} />
+        <Speech facing='right' pos={['150px','650px']} >
+        
+        <Hero pos={['190px','980px']} img={heroImgSky} />
+            <SpeechLib  pitch="0.9"
+                rate="0.9"
+                volume="0.7"
+                lang="en-GB"
+                voice="Microsoft Zira - English (United States)" text='To quote the elder witches, ‘if you want something done well, do it yourself!' />To quote the elder witches, ‘if you want something done well, do it yourself!
+                </Speech>
+        </>
+    }
+
 	decisionList = [
 		//prompt1, // narrator promt
+		dialog1,
+		dialog2,
+		dialog3,
 		decision1,
-		//decision2,
+		stepAdialog1,
+		// stepAdialog2,
+        // stepAdialog3,
+        // stepAdialog4
 	]
 
+    console.log(choice);
     return <div className={parent} 
     style={{backgroundImage:`url(${bg})`, backgroundRepeat:'cover'}}>
 
     {decisionList[step]()}
-
-    {/* <Hero pos={['100px','50px']} move={move} clickToMove={clickToMove} />
-    <Speech pos={['75px','350px']} >
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis impedit aspernatur sit provident consequatur tempore, molestiae dolor quia hic numquam iste iure eligendi. Tenetur cumque veniam porro, atque exercitationem distinctio.
-    </Speech> */}
-
 
     {/* <Button pos={['500px',`${1280/2-215/2}px`]} fn={next}/> */}
 
