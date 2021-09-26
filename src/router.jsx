8784e95b627b2ed02e4418c4ba9b7f5c4a47b371
Chris De Leon <cdeleon.ca91@gmail.com>
2021-09-26 00:23:14 -0400

    added scene 2

--- src/router.jsx
+++ src/router.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Start from "../src/scene/start";
 import Scene1 from "../src/scene/scene1";
+import Scene2 from "../src/scene/scene2";
 
 export default function Index() {
 	return (
 		<Router>
 			<Switch>
-				<Route path="/">
+				<Route exact path="/">
 					<Start />
 				</Route>
-				<Route path="/scene1">
+				<Route exact path="/scene1">
 					<Scene1 />
+				</Route>
+				<Route exact path="/scene2">
+					<Scene2 />
 				</Route>
 			</Switch>
 		</Router>
--- src/scene/start/index.jsx
+++ src/scene/start/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../../store/store";
 import { useHistory } from "react-router-dom";
-import SpeechLib from 'react-speech';
+import SpeechLib from "react-speech";
 
 //import components
 import Timer from "../../components/timer";
@@ -12,7 +12,7 @@ import Speech from "../../components/speech";
 import Pet from "../../components/pet";
 
 // import imgs
-import heroImg from '../../assets/Arouramain.png'
+import heroImg from "../../assets/Arouramain.png";
 
 export default function Index() {
 	let history = useHistory();
@@ -24,32 +24,37 @@ export default function Index() {
 	};
 
 	const next = () => {
-        // console.log('NEXT!')
+		// console.log('NEXT!')
 		// history.push("/test");
 	};
 
 	const decision1 = () => {
+		return (
+			<>
+				<Hero pos={["275px", "30px"]} img={heroImg} />
 
-		return <>
-			<Hero pos={['275px','30px']} img={heroImg} />
+				<Speech facing="left" pos={["300px", "300px"]}>
+					<SpeechLib
+						pitch="0.9"
+						rate="0.9"
+						volume="0.7"
+						lang="en-GB"
+						voice="Microsoft Zira - English (United States)"
+						text="Halloween is coming and I just might have the perfect idea to get the spooky season rolling. A Halloween pop-up store! Now, to decide, brick and mortar or online?
+					"
+					/>
+					Halloween is coming and I just might have the perfect idea to get the
+					spooky season rolling. A Halloween pop-up store! Now, to decide, brick
+					and mortar or online?
+				</Speech>
+			</>
+		);
+	};
 
-			<Speech facing='left' pos={['300px','300px']} >
-				<SpeechLib  pitch="0.9"
-					rate="0.9"
-					volume="0.7"
-					lang="en-GB"
-					voice="Microsoft Zira - English (United States)" text='Halloween is coming and I just might have the perfect idea to get the spooky season rolling. A Halloween pop-up store! Now, to decide, brick and mortar or online?
-					' />
-					Halloween is coming and I just might have the perfect idea to get the spooky season rolling. A Halloween pop-up store! Now, to decide, brick and mortar or online?
-			</Speech>
-		</>
-	}
-
-	
 	const decisionList = [
 		decision1,
-		decision2,
-	]
+		// decision2,
+	];
 
 	// console.log("start: choice-", choice);
 
@@ -61,24 +66,20 @@ export default function Index() {
 			/> */}
 
 			{decision1()}
-			
-
 
 			{/* <Hero pos={['100px','50px']} move={move} clickToMove={clickToMove} />
 			<Speech pos={['75px','350px']} >
 				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis impedit aspernatur sit provident consequatur tempore, molestiae dolor quia hic numquam iste iure eligendi. Tenetur cumque veniam porro, atque exercitationem distinctio.
 			</Speech> */}
-		
 
 			{/* <Button pos={['500px',`${1280/2-215/2}px`]} fn={next}/> */}
 
-            {/* <Tree options={[
+			{/* <Tree options={[
                 'route a',
                 'route b'
             ]} defaultOpt={1} func={getChoice}/> */}
 
-			<Pet pos={['600px',`1175px`]}/>
-
+			<Pet pos={["600px", `1175px`]} />
 		</div>
 	);
 }
